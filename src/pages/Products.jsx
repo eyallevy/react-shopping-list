a1041c1789a1a247bb9111644115cf4709ea51b8
Eyal Levy <eyal.levya@gmail.com>
2024-03-08 06:47:58 +0000

    Section 21 - 332-335 Working with index routes

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,20 @@
 import { Link } from 'react-router-dom';
 
+const PRODUCTS = [
+    { id: 'p1' , title: 'Product 1'},
+    { id: 'p2' , title: 'Product 2'},
+    { id: 'p3' , title: 'Product 3'},
+];
+
 function ProductsPage() {
     return (<>
-        <h1>Products Page</h1>
+        <h1>The Products Page</h1>
         <ul>
-            <li><Link to="/products/p1">Product 1</Link></li>
-            <li><Link to="/products/p2">Product 2</Link></li>
-            <li><Link to="/products/p3">Product 3</Link></li>
+            {PRODUCTS.map((prod) => (
+                <li key={prod.id}>
+                    <Link to={`/products/${prod.id}`}>{prod.title}</Link>
+                </li>
+            ))}
         </ul>
     </>);
 };
